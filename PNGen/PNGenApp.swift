d9547b411040028dc8136a7526603becdb9dc88e
Thomas Arnold <thomas.arnold1@swisscom.com>
2022-02-11 14:12:14 +0100

    Initial Commit

---
+++ PNGen/PNGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PNGenApp.swift
+//  PNGen
+//
+//  Created by taaarth2 on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct PNGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
