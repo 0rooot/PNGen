0db569e9df2dc5be4434945acd70969e8f3dd004
arth <arnold94@gmx.ch>
2022-03-17 11:40:51 +0100

    add Button

--- PNGenApp/Screens/PinkNoiseView.swift
+++ PNGenApp/Screens/PinkNoiseView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
-struct PinkNoiseView: View {
-    var body: some View {
-                Text("Here will apeare a Button that generates Pink Noise!")
-    }
+struct Colors {
+    static let navyBlue = UIColor(red: 0, green: 0, blue: 128.0, alpha: 1.0)
 }
 
+struct PinkNoiseView: View {
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                print("Start Pink Noise Now!")
+            }){
+                Text("Start Pink Noise")
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(Color.black)
+                    .background(Color.red)
+                    .clipShape(Circle())
+            }.buttonStyle(GrowingButton())
+        }.frame(width: 300, height: 500)
+        
+    }
+}
 
 struct PinkNoiseView_Previews: PreviewProvider {
     static var previews: some View {
         PinkNoiseView()
+    }
+}
+
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
