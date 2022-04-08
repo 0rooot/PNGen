05b978e28b77d080565636f1604cf557367c0d8b
arth <arnold94@gmx.ch>
2022-04-08 17:58:09 +0200

    optimize views and make multilingual

--- PNGenApp/Logic/Sound/PlaySound.swift
+++ PNGenApp/Logic/Sound/PlaySound.swift
@@ -8,9 +8,10 @@
 import Foundation
 import AVFoundation
 
-var player: AVAudioPlayer!
+var player1: AVAudioPlayer!
+var player2: AVAudioPlayer!
 
-func playPinkNoise(key: String, format: String){
+func playPinkNoise1(key: String, format: String){
     let url = Bundle.main.url(forResource: key, withExtension: format)
     
     guard url != nil else {
@@ -24,9 +25,31 @@ func playPinkNoise(key: String, format: String){
                          mode: .default,
                          options: .mixWithOthers
             )
-        player = try AVAudioPlayer(contentsOf: url!)
-        player?.play()
-        player.numberOfLoops = -1
+        player1 = try AVAudioPlayer(contentsOf: url!)
+        player1?.play()
+        player1.numberOfLoops = -1
+    } catch {
+        print("error")
+    }
+}
+
+func playPinkNoise2(key: String, format: String){
+    let url = Bundle.main.url(forResource: key, withExtension: format)
+    
+    guard url != nil else {
+        return
+    }
+    
+    do {
+        _ = try? AVAudioSession.sharedInstance()
+            .setCategory(
+                AVAudioSession.Category.playback,
+                         mode: .default,
+                         options: .mixWithOthers
+            )
+        player2 = try AVAudioPlayer(contentsOf: url!)
+        player2?.play()
+        player2.numberOfLoops = -1
     } catch {
         print("error")
     }
@@ -34,5 +57,6 @@ func playPinkNoise(key: String, format: String){
 
 
 func stopPinkNoise(){
-    player?.stop()
+    player1?.stop()
+    player2?.stop()
 }
--- PNGenApp/Screens/PinkNoiseView.swift
+++ PNGenApp/Screens/PinkNoiseView.swift
@@ -12,82 +12,16 @@ struct PinkNoiseView: View {
     
     @State var lang = "DE"
     @State private var showingInformationPopover = false
-    @State var playNoise = false
-    @State var buttonTitle: String = "Start \n Pink Noise"
-    @State var standardBrightness = UIScreen.main.brightness;
     
     var body: some View {
         
         return NavigationView {
             GeometryReader { geometry in
                 VStack {
-                    HStack {
-                        Button(action: {
-                            if (playNoise==false) {
-                                playNoise = true
-                                buttonTitle = "Stop \n Pink Noise"
-                                AudioServicesPlayAlertSoundWithCompletion(SystemSoundID(kSystemSoundID_Vibrate)) {   }
-                                playPinkNoise(key: "PINKNoise", format: "mp3")
-                                UIScreen.main.brightness = CGFloat(0.3)
-                                UIApplication.shared.isIdleTimerDisabled = false
-                            } else {
-                                playNoise = false
-                                buttonTitle = "Start \n Pink Noise"
-                                stopPinkNoise()
-                                UIScreen.main.brightness = standardBrightness
-                                UIApplication.shared.isIdleTimerDisabled = true
-                            }
-                        }){
-                            Text(buttonTitle)
-                                .bold()
-                                .font(.title)
-                                .frame(width: 200, height: 200, alignment: .center)
-                                .multilineTextAlignment(.center)
-                                .clipShape(Circle())
-                        }
-                        .buttonStyle(GrowingButton())
-                        
-                    }
+                    ButtonView()
                     .frame(width: geometry.size.width, height: geometry.size.height/2)
                     
-                    VStack(alignment: .leading) {
-                        Text("Hinweis:")
-                            .font(.title.bold())
-                        Group{
-                            VStack(alignment: .leading) {
-                                HStack{
-                                    Image(systemName: "speaker.wave.2.circle")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 70, height: 70)
-                                    VStack{Text("Stelle die Lautstärke auf Maximum und deaktiviere den Lautlos Modus." )
-                                            .fixedSize(horizontal: false, vertical: true)
-                                            .frame(maxHeight: .infinity, alignment: .leading)
-                                            .font(.system(size: 18.0))
-                                            .padding(.leading, 10)
-                                    }
-                                }
-                            }
-                            VStack(alignment: .leading) {
-                                HStack{
-                                    Image(systemName: "lock.circle")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 70, height: 70)
-                                    VStack{Text("Sperre das Iphone um Akku zu sparen." )
-                                            .fixedSize(horizontal: false, vertical: true)
-                                            .frame(maxHeight: .infinity, alignment: .leading)
-                                            .font(.system(size: 18.0))
-                                            .padding(.leading, 10)
-                                    }
-                                }
-                            }
-                            Spacer()
-                        }
-                        .font(.body)
-                        .lineSpacing(10)
-                        .padding(5)
-                        .font(.subheadline)}
+                    HintView()
                     .frame(width: geometry.size.width/1.1, height: geometry.size.height/2)
                 }
             }
@@ -107,44 +41,7 @@ struct PinkNoiseView: View {
                         .imageScale(.large)
                 }
                     .popover(isPresented: $showingInformationPopover) {
-                        VStack(alignment: .leading) {
-                            Spacer()
-                            Text("Was ist Pink Noise?")
-                                .font(.largeTitle)
-                                .padding(.bottom, 20)
-                            Text("Pink Noise ist der Sound von allen Frequenzen. Pink Noise hat verschiedene Einsatzzwecke. Das Einsatzgebiet dieser App ist die Überwachungs- und Abhör-Abwehr. Pink Noise soll die umliegenden Mikrofone von Smartphones und Smartwatches füllen und die Aufnahme von Gesprächen verhindern." )
-                                .font(.body)
-                                .lineSpacing(10)
-                                .padding(.bottom, 5)
-                            Spacer()
-                            Group{
-                                Text("Optimale Anwendung:")
-                                    .font(.largeTitle)
-                                    .padding(.bottom, 20)
-                                Text("1. Pink Noise mit Button Starten" )
-                                    .font(.body)
-                                    .lineSpacing(10)
-                                    .padding(.bottom, 10)
-                                Text("2. Alle Smartphones und Smartwatches im Raum in das Lauschschutzglas legen" )
-                                    .font(.body)
-                                    .lineSpacing(10)
-                                    .padding(.bottom, 10)
-                                Text("3. Lauschschutz Glas schliessen und Sitzung beginnen." )
-                                    .font(.body)
-                                    .lineSpacing(10)
-                                    .padding(.bottom, 10)
-                            }
-                            Spacer()
-                            Text("Weitere Infos:")
-                                .font(.largeTitle)
-                                .padding(.bottom, 20)
-                            Text("Ich mag Züge" )
-                                .font(.body)
-                                .lineSpacing(10)
-                                .padding(.bottom, 10)
-                            Spacer()
-                        }
-                        .padding(20)
+                        InfoView()
                     }
             )
             )
@@ -159,14 +56,3 @@ struct PinkNoiseView_Previews: PreviewProvider {
     }
 }
 
-struct GrowingButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .background(Color(red: 165 / 255, green: 32 / 255, blue: 25 / 255))
-            .foregroundColor(Color.white)
-            .clipShape(Capsule())
-            .shadow(color: .gray, radius: 2, x: 5, y: 5)
-            .scaleEffect(configuration.isPressed ? 1.1 : 1)
-            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-    }
-}
