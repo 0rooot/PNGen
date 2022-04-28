f73f8d2000c9f94f3bf47a1f5521e5406a892593
arth <arnold94@gmx.ch>
2022-04-28 13:23:11 +0200

    fix formatting and button links

--- PNGenApp/Screens/HomeViews/InfoView.swift
+++ PNGenApp/Screens/HomeViews/InfoView.swift
@@ -44,52 +44,78 @@ struct InfoView: View {
                         }
                     }
                 }
-                Spacer()
-                Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.FurtherInfos.Title"))
-                    .font(.largeTitle)
-                    .padding(.bottom, 20)
-                Spacer()
-                Group {
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.PinkNoiseExplanation.Title"))
-                        .font(.title2)
-                        .foregroundColor(.accentColor)
-                        .padding(.bottom, 5)
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.PinkNoiseExplanation.Body"))
-                        .font(.body)
-                        .lineSpacing(10)
+                Group{
+                    Spacer()
+                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.FurtherInfos.Title"))
+                        .font(.largeTitle)
                         .padding(.bottom, 20)
-                    Divider().background(Color.accentColor).frame(maxWidth: 200)
-                }
-                Spacer()
-                Group {
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Autostart.Title"))
-                        .font(.title2)
-                        .foregroundColor(.accentColor)
-                        .padding(.bottom, 5)
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Autostart.Body"))
-                        .font(.body)
-                        .lineSpacing(10)
-                        .padding(.bottom, 20)
-                    Divider().background(Color.accentColor).frame(maxWidth: 200)
-                }
-                Spacer()
-                Group {
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Jar.Title"))
-                        .font(.title2)
-                        .foregroundColor(.accentColor)
-                        .padding(.bottom, 5)
-                    Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Jar.Body"))
-                        .font(.body)
-                        .lineSpacing(10)
-                        .padding(.bottom, 20)
-                    GeometryReader { geometry in
-                        HStack{
-                            Link("Migros", destination: URL(string: "https://produkte.migros.ch/bormioli-rocco-einmachglas-fido-703723200000")!).foregroundColor(.red).frame(minWidth: 0, maxWidth: geometry.size.width/3)
-                            Link("Galaxus", destination: URL(string: "https://www.galaxus.ch/de/s2/product/bormioli-rocco-fido-1-stk-3-l-einmachglas-3510678")!).foregroundColor(.red).frame(minWidth: 0, maxWidth: geometry.size.width/3)
-                            Link("Manor", destination: URL(string: "https://www.manor.ch/de/p/p0-33862006")!).foregroundColor(.red).frame(minWidth: 0, maxWidth: geometry.size.width/3)
+                    Spacer()
+                    Group {
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.PinkNoiseExplanation.Title"))
+                            .font(.title2)
+                            .foregroundColor(.accentColor)
+                            .padding(.bottom, 5)
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.PinkNoiseExplanation.Body"))
+                            .font(.body)
+                            .lineSpacing(10)
+                            .padding(.bottom, 20)
+                        Divider().background(Color.accentColor).frame(maxWidth: 200)
+                    }
+                    Spacer()
+                    Group {
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Autostart.Title"))
+                            .font(.title2)
+                            .foregroundColor(.accentColor)
+                            .padding(.bottom, 5)
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Autostart.Body"))
+                            .font(.body)
+                            .lineSpacing(10)
+                            .padding(.bottom, 20)
+                        Divider().background(Color.accentColor).frame(maxWidth: 200)
+                    }
+                    Spacer()
+                    Group {
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Jar.Title"))
+                            .font(.title2)
+                            .foregroundColor(.accentColor)
+                            .padding(.bottom, 5)
+                        Text(LocalizedStringKey("PinkNoiseGeneratorView.Info.Jar.Body"))
+                            .font(.body)
+                            .lineSpacing(10)
+                            .padding(.bottom, 20)
+                        GeometryReader { geometry in
+                            HStack{
+                                
+                                Link(destination: URL(string: "https://produkte.migros.ch/bormioli-rocco-einmachglas-fido-703723200000")!, label: {
+                                    Text("Migros")
+                                        .bold()
+                                        .frame(minWidth: 0, maxWidth: geometry.size.width/3, minHeight: 50)
+                                        .foregroundColor(Color(UIColor.systemBackground))
+                                        .background(Color(UIColor.label))
+                                        .cornerRadius(15)
+                                })
+                                Link(destination: URL(string: "https://www.galaxus.ch/de/s2/product/bormioli-rocco-fido-1-stk-3-l-einmachglas-3510678")!, label: {
+                                    Text("Galaxus")
+                                        .bold()
+                                        .frame(minWidth: 0, maxWidth: geometry.size.width/3, minHeight: 50)
+                                        .foregroundColor(Color(UIColor.systemBackground))
+                                        .background(Color(UIColor.label))
+                                        .cornerRadius(15)
+                                })
+                                Link(destination: URL(string: "https://www.manor.ch/de/p/p0-33862006")!, label: {
+                                    Text("Manor")
+                                        .bold()
+                                        .frame(minWidth: 0, maxWidth: geometry.size.width/3, minHeight: 50)
+                                        .foregroundColor(Color(UIColor.systemBackground))
+                                        .background(Color(UIColor.label))
+                                        .cornerRadius(15)
+                                })
+                                
+                            }
                         }
                     }
                 }
+                Spacer()
             }
             .padding(20)
         }
--- PNGenApp/Screens/LearnViews/CyberRulesLearnView.swift
+++ PNGenApp/Screens/LearnViews/CyberRulesLearnView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct CyberRulesLearnView: View {
+    @Environment(\.openURL) var openURL
     @Binding var showMenu: Bool
     
     var body: some View {
-        HStack{
-            Spacer()
-            ScrollView{
+        ScrollView{
+            VStack{
+                Spacer()
+                
                 VStack(alignment: .leading) {
                     Text(LocalizedStringKey("LearnView.CyberRules.Title"))
                         .font(.largeTitle)
@@ -21,76 +23,91 @@ struct CyberRulesLearnView: View {
                         .font(.body)
                     Group{
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+1").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+1").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule1"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+2").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+2").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule2"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+3").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+3").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule3"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+4").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+4").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule4"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+5").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+5").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule5"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+6").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+6").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule6"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                         VStack(alignment: .leading) {
-                            HStack{Image("image.CyD+Regel+7").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
+                            HStack(alignment: .center){Image("image.CyD+Regel+7").resizable().scaledToFill().frame(width: 100, height: 100).clipped()
                                 VStack{Text(LocalizedStringKey("LearnView.CyberRules.Rule7"))
                                         .fixedSize(horizontal: false, vertical: true)
                                         .frame(maxHeight: .infinity, alignment: .top)
+                                        .padding(.leading, 5)
                                         .font(.body)
-                                }
-                            }
+                                }.background(Color(UIColor.systemBackground))
+                            }.background(Color(red: 239 / 255, green: 240 / 255, blue: 241 / 255))
                         }
                     }
-                    Text(LocalizedStringKey("LearnView.CyberRules.VideoHint"))
-                        .font(.subheadline)
-                    Link("Cyber Defence Rules - Admin.ch", destination: URL(string: "https://www.vtg.admin.ch/de/aktuell/themen/cyberdefence/cyber-security.html#18_1621347327946")!).foregroundColor(.red)
-                }
-            }
+                    
+                } //VStack leading
+                Divider().background(Color.accentColor).frame(maxWidth: 200)
+                Text(LocalizedStringKey("LearnView.CyberRules.VideoHint"))
+                    .font(.title2)
+                Link(destination: URL(string: "https://www.vtg.admin.ch/de/aktuell/themen/cyberdefence/cyber-security.html#18_1621347327946")!, label: {
+                    Text("Cyber Defence Rules - Admin.ch")
+                        .bold()
+                        .frame(width: 280, height: 50)
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .background(Color(UIColor.label))
+                        .cornerRadius(15)
+                })
+            } //VStack
             Spacer()
-        }
-        
+        } //Scroll View
     }
 }
